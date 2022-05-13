a85cfa66b0a5b8feae80546fffdf12d1aab2e8b9
zliel <zpliel@gmail.com>
2022-05-12 19:58:38 -0700

    Added methods to retrieve all coins from Redis and fetch their price histories

--- src/main/java/com/personal/cryptorankingservice/service/CoinsDataService.java
+++ src/main/java/com/personal/cryptorankingservice/service/CoinsDataService.java
@@ -1,10 +1,15 @@
 package com.personal.cryptorankingservice.service;
 
-import com.personal.cryptorankingservice.model.Coins;
+import com.personal.cryptorankingservice.model.*;
 import com.personal.cryptorankingservice.utils.HttpUtils;
 import io.github.dengliming.redismodule.redisjson.RedisJSON;
+import io.github.dengliming.redismodule.redisjson.args.GetArgs;
 import io.github.dengliming.redismodule.redisjson.args.SetArgs;
 import io.github.dengliming.redismodule.redisjson.utils.GsonUtils;
+import io.github.dengliming.redismodule.redistimeseries.DuplicatePolicy;
+import io.github.dengliming.redismodule.redistimeseries.RedisTimeSeries;
+import io.github.dengliming.redismodule.redistimeseries.Sample;
+import io.github.dengliming.redismodule.redistimeseries.TimeSeriesOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -12,22 +17,62 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class CoinsDataService {
 
-    private String get_coins_endpoint = "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
+    private String get_all_coins_endpoint = "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
     private String redis_coins_key = "coins";
+    private String base_coin_endpoint = "https://coinranking1.p.rapidapi.com/coin/";
+    private String history_endpoint_addon = "/history?timePeriod=";
+    private List<String> timePeriods = List.of("24h", "7d", "30d", "3m", "1y", "3y", "5y");
 
     @Autowired
     private RestTemplate template;
     @Autowired
     private RedisJSON redisJSON;
+    @Autowired
+    private RedisTimeSeries redisTimeSeries;
 
     public void getCoins() {
       log.info("Getting coin data");
-      ResponseEntity<Coins> response = template.exchange(get_coins_endpoint, HttpMethod.GET, HttpUtils.getHttpEntity(), Coins.class);
+      ResponseEntity<Coins> response = template.exchange(get_all_coins_endpoint, HttpMethod.GET, HttpUtils.getHttpEntity(), Coins.class);
       storeCoins(response.getBody());
+    }
+
+    public void getCoinHistory() {
+        log.info("Fetching Coin History");
+        List<CoinInfo> allCoins = getAllCoinsFromRedisJSON();
+        allCoins.forEach(coin -> {
+            timePeriods.forEach(period -> {
+                getCoinHistoryByTimePeriod(coin, period);
+            });
+        });
+    }
+
+    public void getCoinHistoryByTimePeriod(CoinInfo coinInfo, String period) {
+        log.info("Fetching History of " + coinInfo.getName() + " for Time Period " + period);
+        String fullUrl = base_coin_endpoint + coinInfo.getUuid() + history_endpoint_addon + period;
+        ResponseEntity<CoinPriceHistory> coinPriceHistoryEntity = template.exchange(fullUrl, HttpMethod.GET, HttpUtils.getHttpEntity(), CoinPriceHistory.class);
+        storeCoinToTimeSeries(coinPriceHistoryEntity.getBody(), coinInfo.getSymbol(), period);
+    }
+
+    public void storeCoinToTimeSeries(CoinPriceHistory coinPriceHistory, String symbol, String period) {
+        List<CoinPriceHistoryExchangeRate> coinExchangeRateHistory = coinPriceHistory.getData().getHistory();
+
+        coinExchangeRateHistory.stream().filter(rate -> rate.getPrice() != null && rate.getTimestamp() != null)
+                .forEach(rate -> redisTimeSeries.add(new Sample(symbol + ":" + period, Sample.Value.of(Long.parseLong(rate.getTimestamp()),
+                        Double.parseDouble(rate.getPrice()))),
+                        new TimeSeriesOptions()
+                                .unCompressed()
+                                .duplicatePolicy(DuplicatePolicy.LAST)));
+    }
+
+    public List<CoinInfo> getAllCoinsFromRedisJSON() {
+        CoinData coinData = redisJSON.get(redis_coins_key, CoinData.class, new GetArgs().path(".data").indent("\t").newLine("\n").space(" "));
+        return coinData.getCoins();
     }
 
     private void storeCoins(Coins coins) {
